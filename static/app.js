060b5d0384fbd84916196b058c5316e18f123952
strawbrea <breannalowery8@gmail.com>
2024-09-02 22:55:19 -0400

    base

---
+++ static/app.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            newQuote: {
+                text: '',
+                author: '',
+                public: false,
+                disableComments: false
+            },
+            quotes: []
+        };
+    },
+    methods: {
+        addQuote() {
+            fetch('/api/add_quote', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.newQuote)
+            })
+            .then(response => response.json())
+            .then(data => {
+                window.location.href = '/quotes'; 
+                if (!data.success) {
+                    alert('Issue creating new post. ' + (data.error || 'Unknown error'));
+                }
+            })
+            .catch(error => {
+                window.location.href = '/quotes'; 
+                console.error('Error adding quote:', error);
+                alert('Issues encountered while creating new post.');
+            });
+        }
+    }
+}).mount('#app');
